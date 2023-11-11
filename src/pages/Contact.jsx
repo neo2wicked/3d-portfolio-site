c1e911e3f202a586899a44bf811c16ad7db7edff
neo2wicked <capricorn@Live.com>
2023-11-11 12:44:50 -0600

    updated finishing touches on homepage and contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,14 +15,14 @@ import {
           Hello, I'm{" "}
           <span className='blue-gradient_text font-semibold drop-shadow'>
             {" "}
-            Adrian
+            Amani
           </span>{" "}
           👋
         </h1>
   
         <div className='mt-5 flex flex-col gap-3 text-slate-500'>
           <p>
-            Software Engineer based in Croatia, specializing in technical
+            Software Engineer based in Texas, specializing in technical
             education through hands-on learning and building applications.
           </p>
         </div>
